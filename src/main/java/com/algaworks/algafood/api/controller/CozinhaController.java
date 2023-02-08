93890f7bcd2fe5645bedb549f32717865b775e85
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-02-08 15:23:57 -0300

    4.17. Conhecendo os métodos HTTP

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,13 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
@@ -18,10 +19,20 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
-	@GetMapping
-	public List<Cozinha> listar(){
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
 	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXML() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		return cozinhaRepository.buscar(id);
+	}
+
 }
