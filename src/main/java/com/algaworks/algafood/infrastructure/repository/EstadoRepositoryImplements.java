fd3a5caff2b3379a72e1469b64bb0857f8b4beed
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-02-15 10:22:37 -0300

    4.32. Desafio: implementando serviços REST de cidades e estados

--- src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImplements.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImplements.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,8 +38,13 @@ public class EstadoRepositoryImplements implements EstadoRepository{
 
 	@Transactional
 	@Override
-	public void remover(Estado estado) {
-		estado = buscar(estado.getId());
+	public void remover(Long id) {
+		Estado estado = buscar(id);
+		
+		if(estado == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
 		manager.remove(estado);
 	}
 
