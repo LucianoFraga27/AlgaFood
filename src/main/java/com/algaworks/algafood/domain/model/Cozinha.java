519aa9b1071e36d852d5318414252e43e59e46c7
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-03-13 15:06:05 -0300

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany - Error

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -28,5 +32,10 @@ public class Cozinha {
 	@JsonProperty(value = "nome")
 	@Column(nullable=false)
 	private String nome;
+	
+	// Uma cozinha tem muitos restaurante
+	//@JsonIgnore // Para envitar essa propriedade para que não entre num loop circular
+	@OneToMany(mappedBy="cozinha")  // Querendo saber qual nome do atributo mapeado em Restaurante
+	private List<Restaurante> restaurantes = new ArrayList<>();
 	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,3 +10,6 @@ insert into restaurante (nome,taxa_frete,cozinha_id) values ('Churrascaria',10.0
 insert into estado (nome) values ('rio de janeiro');
 insert into estado (nome) values ('minas gerais');
 insert into estado (nome) values ('sao paulo');
+
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
