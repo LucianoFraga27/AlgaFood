f8f8778779fdf4b025f5ae7669956dff9a04aad9
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-03-14 09:58:40 -0300

    6.2. Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,5 +11,8 @@ insert into estado (nome) values ('rio de janeiro');
 insert into estado (nome) values ('minas gerais');
 insert into estado (nome) values ('sao paulo');
 
+insert into forma_pagamento (descricao) values ('Crédito');
+insert into forma_pagamento (descricao) values ('Débito');
+insert into forma_pagamento (descricao) values ('PIX');
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
