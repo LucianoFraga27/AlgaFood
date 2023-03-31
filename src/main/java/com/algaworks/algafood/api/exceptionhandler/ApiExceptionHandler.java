54e9c991bd33c82f5edaefa86d4e9fe880f3e3dc
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-03-31 10:49:06 -0300

    tratando a PropertyBindingException na desserialização

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,73 +16,110 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
-	
+
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
+
 		Throwable rootCause = ExceptionUtils.getRootCause(ex);
-		
-		if(rootCause instanceof InvalidFormatException) {
-			return handleInvalidFormatException((InvalidFormatException)rootCause,headers,status,request);
+
+		if (rootCause instanceof InvalidFormatException) {
+			return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
 		}
-		
+
+		if (rootCause instanceof IgnoredPropertyException) {
+			return handleIgnoredPropertyException((IgnoredPropertyException) rootCause, headers, status, request);
+		}
+
+		if (rootCause instanceof UnrecognizedPropertyException) {
+			return handleUnrecognizedPropertyException((UnrecognizedPropertyException) rootCause, headers, status,
+					request);
+		}
+
 		ProblemType problemType = ProblemType.MESANGEM_INCOMPREENSIVEL;
-		
+
 		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
-		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
-	}
-	
-	
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
-		String path = ex.getPath().stream()
-								  .map(ref -> ref.getFieldName())
-								  .collect(Collectors.joining("."));
-		
-		ProblemType problemType = ProblemType.MESANGEM_INCOMPREENSIVEL;
-		String detail = String.format("A propriedade ['%s'] recebeu o valor ['%s'], "
-				+ "que é de um tipo inválido. Corrija e informe o valor compatível com o tipo ['%s'].",
-				path,
-				ex.getValue(),
-				ex.getTargetType().getSimpleName());
-		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
+	private ResponseEntity<Object> handleUnrecognizedPropertyException(UnrecognizedPropertyException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		String path = ex.getPath().stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+
+		ProblemType problemType = ProblemType.MESANGEM_INCOMPREENSIVEL;
+
+		String detail = String.format("O campo ['%s'] não existe.", path);
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+	private ResponseEntity<Object> handleIgnoredPropertyException(IgnoredPropertyException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ProblemType problemType = ProblemType.MESANGEM_INCOMPREENSIVEL;
+
+		String detail = String.format("O campo ['%s'] não deve ser incluso em sua requisição ", ex.getPropertyName());
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+	private String joinPath(List<Reference> references) {
+		return references.stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+	}
+
+	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		String path = joinPath(ex.getPath());
+
+		ProblemType problemType = ProblemType.MESANGEM_INCOMPREENSIVEL;
+		String detail = String.format(
+				"A propriedade ['%s'] recebeu o valor ['%s'], "
+						+ "que é de um tipo inválido. Corrija e informe o valor compatível com o tipo ['%s'].",
+				path, ex.getValue(), ex.getTargetType().getSimpleName());
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
 
 	@ExceptionHandler({ EntidadeNaoEncontradaException.class })
 	public ResponseEntity<?> handleEstadoNaoEncontradoException(WebRequest request, EntidadeNaoEncontradaException ex) {
-		
+
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
 		String detail = ex.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
 	}
 
 	@ExceptionHandler({ EntidadeEmUsoException.class })
 	public ResponseEntity<?> handleEntidadeEmUsoException(WebRequest request, EntidadeEmUsoException ex) {
-		
+
 		HttpStatus status = HttpStatus.CONFLICT;
 		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
 		String detail = ex.getMessage();
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
-		return handleExceptionInternal(ex, problem, new HttpHeaders(),status, request);
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
 	}
 
@@ -94,7 +130,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
 		String detail = ex.getMessage();
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 
 	}
@@ -104,28 +140,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpStatus status, WebRequest request) {
 
 		if (body == null) {
-			body = Problem.builder()
-					.status(status.value())
-					.title(status.getReasonPhrase())
-					.build();
+			body = Problem.builder().status(status.value()).title(status.getReasonPhrase()).build();
 		} else if (body instanceof String) {
-			body = Problem.builder()
-					.status(status.value())
-					.title( (String) body )
-					.build();
+			body = Problem.builder().status(status.value()).title((String) body).build();
 		}
 
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
-	
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
-		return Problem
-				.builder()
-				.status(status.value())
-				.type(problemType.getUri())
-				.title(problemType.getTitle())
+		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
 				.detail(detail);
 	}
-	
+
 }
