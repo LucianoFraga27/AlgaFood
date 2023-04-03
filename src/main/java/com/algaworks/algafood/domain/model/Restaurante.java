6345a3b8ae53b19cfc6274ac7244e9bc19a77423
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-04-03 16:30:55 -0300

    9.6. Validando as associações de uma entidade em cascata

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,13 +16,15 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -38,16 +39,19 @@ public class Restaurante {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	@NotBlank
 	@Column(nullable=false)
 	private String nome;
 	
+	@DecimalMin("1")
 	@Column(name="taxa_frete",nullable=false)
 	private BigDecimal taxaFrete;
 
 	
 	// @JsonIgnore
 	// @JsonIgnoreProperties({"hibernateLazyInitializer"})
+	@Valid
+	@NotNull
 	@ManyToOne // (fetch=FetchType.LAZY)
 	@JoinColumn(name="cozinha_id",nullable=false)
 	private Cozinha cozinha;
